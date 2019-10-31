4784fda6d421c90cfa3ee012d4d02d9928e4c187
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-31 12:58:24 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+class Cat 
+  def meow
+    puts "meow!"
+  end 
+  
+  def name=(animal_name) 
+    @name = animal_name
+  end
+  
+end
+
+maru = Cat.new 
+maru.name= maru
+maru.meow
